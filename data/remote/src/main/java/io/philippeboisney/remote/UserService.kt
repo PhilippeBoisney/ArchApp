d858e9a6c36cffe84aba6f57c2c581adad3ebe22
hamedsilver <hamed.safari.hs@gmail.com>
2020-03-26 21:36:56 +0430

    Update UserService.kt
    💃

--- data/remote/src/main/java/io/philippeboisney/remote/UserService.kt
+++ data/remote/src/main/java/io/philippeboisney/remote/UserService.kt
@@ -10,7 +10,7 @@ import retrofit2.http.Query
 interface UserService {
 
     @GET("search/users")
-    fun fetchTopUsersAsync(@Query("q") query: String = "hamedsilver",
+    fun fetchTopUsersAsync(@Query("q") query: String = "PhilippeB",
                            @Query("sort") sort: String = "followers"): Deferred<ApiResult<User>>
 
     @GET("users/{login}")
